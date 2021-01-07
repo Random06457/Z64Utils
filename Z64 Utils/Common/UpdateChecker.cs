62d61da0578be4418126c4f9223043e3a901d48e
Random <28494085+Random06457@users.noreply.github.com>
2021-01-07 11:39:35 +0100

    bump version

--- Z64 Utils/Common/UpdateChecker.cs
+++ Z64 Utils/Common/UpdateChecker.cs
@@ -10,7 +10,7 @@ namespace Common
     public static class UpdateChecker
     {
         public const string ReleaseURL = @"https://github.com/Random06457/Z64Utils/releases";
-        public const string CurrentTag = "v1.0.3";
+        public const string CurrentTag = "v1.0.4";
         public static string GetLatestTag()
         {
             HttpWebRequest req = (HttpWebRequest)WebRequest.Create($"{ReleaseURL}/latest");
