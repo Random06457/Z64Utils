c48564a2cb8f7dc93e71ba01850564c1ae4b5205
Random <28494085+Random06457@users.noreply.github.com>
2021-05-02 16:28:11 +0200

    use a PropertyGrid for disas options

--- Z64Utils/Forms/DisasmSettingsForm.Designer.cs
+++ Z64Utils/Forms/DisasmSettingsForm.Designer.cs
@@ -30,12 +30,8 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DisasmSettingsForm));
             this.previewTextBox = new System.Windows.Forms.TextBox();
-            this.checkBoxStatic = new System.Windows.Forms.CheckBox();
-            this.checkBoxShowAddr = new System.Windows.Forms.CheckBox();
-            this.checkBoxRelativeAddr = new System.Windows.Forms.CheckBox();
-            this.checkBoxMultiCmdMacro = new System.Windows.Forms.CheckBox();
-            this.checkBoxAddrLiteral = new System.Windows.Forms.CheckBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.propertyGrid1 = new System.Windows.Forms.PropertyGrid();
             this.SuspendLayout();
             // 
             // previewTextBox
@@ -48,56 +44,6 @@
             this.previewTextBox.Size = new System.Drawing.Size(290, 48);
             this.previewTextBox.TabIndex = 0;
             // 
-            // checkBoxStatic
-            // 
-            this.checkBoxStatic.AutoSize = true;
-            this.checkBoxStatic.Location = new System.Drawing.Point(27, 93);
-            this.checkBoxStatic.Name = "checkBoxStatic";
-            this.checkBoxStatic.Size = new System.Drawing.Size(108, 17);
-            this.checkBoxStatic.TabIndex = 1;
-            this.checkBoxStatic.Text = "Static Commands";
-            this.checkBoxStatic.UseVisualStyleBackColor = true;
-            // 
-            // checkBoxShowAddr
-            // 
-            this.checkBoxShowAddr.AutoSize = true;
-            this.checkBoxShowAddr.Location = new System.Drawing.Point(27, 17);
-            this.checkBoxShowAddr.Name = "checkBoxShowAddr";
-            this.checkBoxShowAddr.Size = new System.Drawing.Size(94, 17);
-            this.checkBoxShowAddr.TabIndex = 2;
-            this.checkBoxShowAddr.Text = "Show Address";
-            this.checkBoxShowAddr.UseVisualStyleBackColor = true;
-            // 
-            // checkBoxRelativeAddr
-            // 
-            this.checkBoxRelativeAddr.AutoSize = true;
-            this.checkBoxRelativeAddr.Location = new System.Drawing.Point(27, 36);
-            this.checkBoxRelativeAddr.Name = "checkBoxRelativeAddr";
-            this.checkBoxRelativeAddr.Size = new System.Drawing.Size(106, 17);
-            this.checkBoxRelativeAddr.TabIndex = 3;
-            this.checkBoxRelativeAddr.Text = "Relative Address";
-            this.checkBoxRelativeAddr.UseVisualStyleBackColor = true;
-            // 
-            // checkBoxMultiCmdMacro
-            // 
-            this.checkBoxMultiCmdMacro.AutoSize = true;
-            this.checkBoxMultiCmdMacro.Location = new System.Drawing.Point(27, 55);
-            this.checkBoxMultiCmdMacro.Name = "checkBoxMultiCmdMacro";
-            this.checkBoxMultiCmdMacro.Size = new System.Drawing.Size(190, 17);
-            this.checkBoxMultiCmdMacro.TabIndex = 4;
-            this.checkBoxMultiCmdMacro.Text = "Recognize Multi Command Macros";
-            this.checkBoxMultiCmdMacro.UseVisualStyleBackColor = true;
-            // 
-            // checkBoxAddrLiteral
-            // 
-            this.checkBoxAddrLiteral.AutoSize = true;
-            this.checkBoxAddrLiteral.Location = new System.Drawing.Point(27, 74);
-            this.checkBoxAddrLiteral.Name = "checkBoxAddrLiteral";
-            this.checkBoxAddrLiteral.Size = new System.Drawing.Size(145, 17);
-            this.checkBoxAddrLiteral.TabIndex = 5;
-            this.checkBoxAddrLiteral.Text = "Show Address As Literals";
-            this.checkBoxAddrLiteral.UseVisualStyleBackColor = true;
-            // 
             // label1
             // 
             this.label1.AutoSize = true;
@@ -107,17 +53,23 @@
             this.label1.TabIndex = 6;
             this.label1.Text = "Preview:";
             // 
+            // propertyGrid1
+            // 
+            this.propertyGrid1.HelpVisible = false;
+            this.propertyGrid1.Location = new System.Drawing.Point(12, 10);
+            this.propertyGrid1.Name = "propertyGrid1";
+            this.propertyGrid1.Size = new System.Drawing.Size(290, 105);
+            this.propertyGrid1.TabIndex = 7;
+            this.propertyGrid1.ToolbarVisible = false;
+            this.propertyGrid1.PropertyValueChanged += new System.Windows.Forms.PropertyValueChangedEventHandler(this.DisassemblyOptionForm_OnOptionUpdate);
+            // 
             // DisasmSettingsForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(314, 188);
+            this.Controls.Add(this.propertyGrid1);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.checkBoxAddrLiteral);
-            this.Controls.Add(this.checkBoxMultiCmdMacro);
-            this.Controls.Add(this.checkBoxRelativeAddr);
-            this.Controls.Add(this.checkBoxShowAddr);
-            this.Controls.Add(this.checkBoxStatic);
             this.Controls.Add(this.previewTextBox);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -135,11 +87,7 @@
         #endregion
 
         private System.Windows.Forms.TextBox previewTextBox;
-        private System.Windows.Forms.CheckBox checkBoxStatic;
-        private System.Windows.Forms.CheckBox checkBoxShowAddr;
-        private System.Windows.Forms.CheckBox checkBoxRelativeAddr;
-        private System.Windows.Forms.CheckBox checkBoxMultiCmdMacro;
-        private System.Windows.Forms.CheckBox checkBoxAddrLiteral;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PropertyGrid propertyGrid1;
     }
 }
--- Z64Utils/Forms/DisasmSettingsForm.cs
+++ Z64Utils/Forms/DisasmSettingsForm.cs
@@ -35,33 +35,15 @@ namespace Z64.Forms
         {
             InitializeComponent();
 
-
-            checkBoxShowAddr.Checked = F3DZEX.Disassembler.StaticConfig.ShowAddress;
-            checkBoxRelativeAddr.Checked = F3DZEX.Disassembler.StaticConfig.RelativeAddress;
-            checkBoxMultiCmdMacro.Checked = F3DZEX.Disassembler.StaticConfig.DisasMultiCmdMacro;
-            checkBoxAddrLiteral.Checked = F3DZEX.Disassembler.StaticConfig.AddressLiteral;
-            checkBoxStatic.Checked = F3DZEX.Disassembler.StaticConfig.Static;
-
-            checkBoxShowAddr.CheckedChanged += new EventHandler(DisassemblyOptionForm_OnOptionUpdate);
-            checkBoxRelativeAddr.CheckedChanged += new EventHandler(DisassemblyOptionForm_OnOptionUpdate);
-            checkBoxMultiCmdMacro.CheckedChanged += new EventHandler(DisassemblyOptionForm_OnOptionUpdate);
-            checkBoxAddrLiteral.CheckedChanged += new EventHandler(DisassemblyOptionForm_OnOptionUpdate);
-            checkBoxStatic.CheckedChanged += new EventHandler(DisassemblyOptionForm_OnOptionUpdate);
-
             byte[] dlist = new byte[] { 0x01, 0x01, 0x20, 0x24, 0x06, 0x00, 0x0F, 0xC8 };
             _disas = new F3DZEX.Disassembler(new F3DZEX.Command.Dlist(dlist, 0x060002C8));
 
-            DisassemblyOptionForm_OnOptionUpdate(this, EventArgs.Empty);
+            propertyGrid1.SelectedObject = F3DZEX.Disassembler.StaticConfig;
+            DisassemblyOptionForm_OnOptionUpdate(null, null);
         }
 
-        private void DisassemblyOptionForm_OnOptionUpdate(object sender, EventArgs e)
+        private void DisassemblyOptionForm_OnOptionUpdate(object sender, PropertyValueChangedEventArgs e)
         {
-            F3DZEX.Disassembler.StaticConfig.ShowAddress = checkBoxShowAddr.Checked;
-            F3DZEX.Disassembler.StaticConfig.RelativeAddress= checkBoxRelativeAddr.Checked;
-            F3DZEX.Disassembler.StaticConfig.DisasMultiCmdMacro = checkBoxMultiCmdMacro.Checked;
-            F3DZEX.Disassembler.StaticConfig.AddressLiteral = checkBoxAddrLiteral.Checked;
-            F3DZEX.Disassembler.StaticConfig.Static = checkBoxStatic.Checked;
-
             Application.OpenForms.OfType<DisasmForm>().ToList().ForEach(f => f.UpdateDisassembly());
             Application.OpenForms.OfType<ObjectAnalyzerForm>().ToList().ForEach(f => f.UpdateDisassembly());
 
